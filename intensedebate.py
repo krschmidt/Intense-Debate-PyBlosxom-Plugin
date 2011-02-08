54706f0ee7a1b46e5007020c9f8c6d8613ec1398
Kyle Schmidt <ks@kyleschmidt.com>
2011-02-07 19:28:58 -0500

    Addded Comment Count
    $commentCount can be used to get a count of comments for the current post. Does not work unless viewing only a single post.

--- intensedebate.py
+++ intensedebate.py
@@ -12,11 +12,14 @@ is the long string after idcomments_acct in the javascript IntenseDebate
 gives you. Then simply put the variable $intenseDebate where you want 
 your comments to appear.
 
+You can also use the variable $commentCount to get a count of the number of
+comments in the story, but this variable will only be populated if we're
+looking at a single story. Finding the number of comments for each story
+while looking at a collection of stories is beyond my current understanding of
+PyBlosxom.
+
 Thank you Jordan Sissel (http://www.semicomplete.com) for your page
 title plugin, which I used to develop this plugin.
-
-I didn't bother with the number of comments script, since I don't currently
-use it.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -52,6 +55,8 @@ def cb_prepare(args):
   data = request.getData()
   if data.has_key("entry_list"):
     if len(data["entry_list"]) == 1:
-      data["intenseDebate"] = "<script>var idcomments_acct = \'" + config['intense_debate_id'] + "\'; var idcomments_post_id; var idcomments_post_url; </script> <span id=\"IDCommentsPostTitle\" style=\"display:none\"></span> <script type=\'text/javascript\' src=\'http://www.intensedebate.com/js/genericCommentWrapperV2.js\'></script>"
+        data["intenseDebate"] = "<script>var idcomments_acct = \'" + config['intense_debate_id'] + "\'; var idcomments_post_id; var idcomments_post_url; </script> <span id=\"IDCommentsPostTitle\" style=\"display:none\"></span> <script type=\'text/javascript\' src=\'http://www.intensedebate.com/js/genericCommentWrapperV2.js\'></script>"
+        data["commentCount"] = "<script>var idcomments_acct = \'" + config['intense_debate_id'] + "\';var idcomments_post_id; var idcomments_post_url;</script> <script type=\"text/javascript\" src=\"http://www.intensedebate.com/js/genericLinkWrapperV2.js\"></script>"
     elif len(data["entry_list"]) > 1:
-      data["intenseDebate"] = ""
+        data["intenseDebate"] = ""
+       
